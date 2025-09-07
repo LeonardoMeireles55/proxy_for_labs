c325b254eb96b63bb8ae8d89a3d2ab71845cfaf0
leonardomeireles55 <leomeireles55@hotmail.com>
2025-09-07 14:32:21 -0300

    refactor: Adiciona verificação para mensagens de controle de qualidade no conversor HL7

--- src/handlers/hl7/helpers/HL7-handle-buffer.js
+++ src/handlers/hl7/helpers/HL7-handle-buffer.js
@@ -1,34 +1,9 @@
 const { HL7_FRAMING } = require('../../utils/buffers');
 const { processHL7Message } = require('./HL7-message-handler')
 
-let allocatedBuffer = Buffer.alloc(0);
-
 const handleBuffer = (buffer, socket) => {
-  if (buffer && buffer.length > 0) {
-    allocatedBuffer = Buffer.concat([allocatedBuffer, buffer]);
+
+    processHL7Message(buffer, socket);
   }
 
-  while (true) {
-     let endIndex = allocatedBuffer.indexOf(HL7_FRAMING.END_BLOCK);
-
-    if (endIndex == -1) {
-      endIndex = allocatedBuffer.indexOf(HL7_FRAMING.END_BLOCK_CR);
-
-      if (endIndex == -1) {
-      break;
-    }
-
-    const completeMessage = allocatedBuffer.subarray(
-      0,
-      endIndex + HL7_FRAMING.END_BLOCK.length
-    );
-
-    allocatedBuffer = allocatedBuffer.subarray(
-      endIndex + HL7_FRAMING.END_BLOCK.length
-    );
-
-    processHL7Message(completeMessage, socket);
-  }
-};
-}
 module.exports = { handleBuffer };
